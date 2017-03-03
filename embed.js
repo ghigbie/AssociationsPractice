be522661587d141a605f3c5775267708fc8c7d81
ghigbie <gehigbie@gmail.com>
2017-03-03 23:46:26 +0000

    added two schemas

--- embed.js
+++ embed.js
@@ -1,2 +1,16 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo");
+
+
+//USER - email, name
+var userSchema = new mongoose.Schema({
+   email: String,
+   name: String
+});
+
+//POST - title, content
+
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
