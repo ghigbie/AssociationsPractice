74d25c86775011559d91da9533b17f93fe7c5f71
ghigbie <gehigbie@gmail.com>
2017-03-04 23:27:29 +0000

    added post

--- references.js
+++ references.js
@@ -11,7 +11,35 @@ var Post = mongoose.model("Post", postSchema);
 var userSchema = new mongoose.Schema({
     email: String,
     name: String,
-    post: [postSchema]
+    post: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post"
+        
+        }
+    ]
 });
 
 var User = mongoose.model("User", userSchema);
+
+User.create({
+    email: "bob@bob.com",
+    name: "Boby McGee"
+}, function(err, user){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(user);
+    }
+});
+
+Post.create({
+    title: "How too cook your shoes.",
+    content: "Meow!"
+}, function(err, post){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(post);
+    }
+});
