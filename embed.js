f251cb3a27db1ddc7df9ee888adcfbaef7df4986
ghigbie <gehigbie@gmail.com>
2017-03-04 00:05:08 +0000

    working embed.js file

--- embed.js
+++ embed.js
@@ -1,12 +1,13 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo");
 
-
 //USER - email, name
 var userSchema = new mongoose.Schema({
    email: String,
    name: String
 });
+
+var User = mongoose.model("User", userSchema);
 
 //POST - title, content
 var postSchema = new mongoose.Schema({
@@ -14,4 +15,18 @@ var postSchema = new mongoose.Schema({
     content: String
 });
 
+
 var postModel = mongoose.model("Post", postSchema);
+
+var newUser = new User({
+    email: "charlie@brown.edu",
+    name: "Charlie Brown"
+});
+
+newUser.save(function(err, user){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(user);
+    }
+});
