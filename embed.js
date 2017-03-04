41eff00be9e355517b70dc276616438053c23f57
ghigbie <gehigbie@gmail.com>
2017-03-04 00:29:28 +0000

    working - associates posts with new user

--- embed.js
+++ embed.js
@@ -1,30 +1,32 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo");
 
+
+//POST - title, content - this needs to be defined first
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+});
+
+var Post = mongoose.model("Post", postSchema);
+
+
 //USER - email, name
 var userSchema = new mongoose.Schema({
-   email: String,
-   name: String
+    email: String,
+    name: String,
+    posts: [postSchema] //telling mongoose to use an array of post and associate with a user
 });
 
 var User = mongoose.model("User", userSchema);
 
-//POST - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String,
-    posts: [postSchema] //telling mongoose to 
-});
-
-
-var Post = mongoose.model("Post", postSchema);
 
 var newUser = new User({
     email: "charlie@brown.edu",
-    name: "Charlie Brown"
+    name: "Charlie Brown II"
 });
 
-newUser.post.push({
+newUser.posts.push({
    title: "How to tame your beagle",
    content: "Just kidding. Snoopy will never obey anyone!"
 });
